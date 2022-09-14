977d4306e7f48b32abeeda8478fec2ac899d55b6
Rogelio Rast <rrast@at-j4.msk.21-school.ru>
2022-09-14 15:44:56 +0300

    all done

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -2,17 +2,25 @@
 #define MUTANTSRACK_HPP
 
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <stack>
+#include <list>
 
 template<class T>
-class MutantStack: public std::stack<T>, public std::vector<T>
+class MutantStack: public std::stack<T>
 {
-private:
-	/* data */
 public:
-	MutantStack();
-	~MutantStack();
+	MutantStack(): std::stack<T>(){};
+	MutantStack(const MutantStack &other){*this = other;}
+	MutantStack& operator=(const MutantStack &other){std::stack<T>::operator=(other); return *this;}
+	~MutantStack(){};
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	iterator	begin(){return this->c.begin();}
+	iterator	end(){return this->c.end();}
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	reverse_iterator	rbegin(){return this->c.rbegin();}
+	reverse_iterator	rend(){return this->c.rend();}
 };
 
 #endif
